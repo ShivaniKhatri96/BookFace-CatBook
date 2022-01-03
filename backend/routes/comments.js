7e6efa3dea12d5134378bdaaaf3cbb80ce76a553
Dan-DH <daniel.diaz.hdez@gmail.com>
2022-01-03 20:05:17 +0100

    set API routes

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-//backend/routes/users
-// get /users           -returns list of all users
-// get /users/:login    -returns information of a specific user
-// post /users/         -creates a new user
-// delete /users/:login -deletes user
+// get /comments/       -returns list of all comments
+// get /comments/:id    -returns information of a specific comment
+// post /comments/      -creates a new comment
+// delete /comments/:id -deletes comment
 
 module.exports = router;
