4bd7aed7f332fd8cac8e306d1dbca0e4da32f439
Dan-DH <daniel.diaz.hdez@gmail.com>
2022-01-17 13:00:59 +0100

    logout done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,32 @@
 // import './App.css';
-import * as React from 'react';
-import Home from './Components/pages/Home';
-import LogIn from './Components/pages/LogIn';
-import Profile from './Components/pages/Profile';
-import SignUp from './Components/pages/Signup';
-import Settings from './Components/pages/Settings';
-import Notifications from './Components/pages/Notifications';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import * as React from "react";
+import Home from "./Components/pages/Home";
+import LogIn from "./Components/pages/LogIn";
+import Profile from "./Components/pages/Profile";
+import SignUp from "./Components/pages/Signup";
+import Settings from "./Components/pages/Settings";
+import Notifications from "./Components/pages/Notifications";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import { useState, useEffect } from 'react';
-import { Navigate } from 'react-router';
+import { useState, useEffect } from "react";
+import { Navigate } from "react-router";
 
 function App() {
-  const [auth, setAuth] = useState('');
+  document.title = "The Catbook";
+  const [auth, setAuth] = useState("");
   useEffect(() => {
     (async () => {
-      const response = await fetch('/authentication').then((res) => res.text());
+      const response = await fetch("/authentication").then((res) => res.text());
       setAuth(response);
     })();
-    
   }, [auth]);
 
-  
-  
-  if (auth === 'Authenticated') {
+  if (auth === "Authenticated") {
     return (
       <div className="page-container">
-        <Navbar auths={setAuth}/>
+        <Navbar auths={setAuth} />
         <Routes>
           <Route exact path="/" element={<Home />} />
           {/* <Route exact path="/Login" element={<LogIn />} /> */}
@@ -36,18 +34,17 @@ function App() {
           <Route exact path="/Profile" element={<Profile />} />
           <Route exact path="/Settings" element={<Settings />} />
           <Route exact path="/Notifications" element={<Notifications />} />
-            {/* this is a  way to redirect to home page when people try to access unathorized links */}
+          {/* this is a  way to redirect to home page when people try to access unathorized links */}
           <Route path="*" element={<Navigate replace to="/" />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </div>
     );
-  } 
-  else {
+  } else {
     return (
       <div className="page-container">
         <Routes>
-        {/* <Route
+          {/* <Route
             exact
             path="/"
             element={<LogIn auth={auth} setAuth={setAuth} />}
